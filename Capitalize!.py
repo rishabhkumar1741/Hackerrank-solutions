2649e9f0b88289af219583aa1a35a9c83d155e8d
Rishabh Kumar <1741rishabh@hmail.com>
2020-05-24 18:37:25 +0530

    capitalize program

---
+++ Capitalize!.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    s = s.split(" ")
+    rishabh = ""
+    for x in s:
+        rishabh += x.capitalize()+' ' 
+    return rishabh
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
